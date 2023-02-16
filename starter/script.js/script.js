4ec837c01ad6c2a2718d2ce7aa06c9b45386da0e
janeezy <janeezyofficial@gmail.com>
2023-02-16 16:10:31 +0000

    styling
    styling

--- starter/script.js/script.js
+++ starter/script.js/script.js
@@ -1,15 +1,1 @@
 'use script';
-console.log(document.querySelector('.logo1').textContent);
-document.onslotchange('div').listener;
-document.querySelector('.logo1').textContent = addEventListener(
-	alert('welcome to my page '),
-	listener('welcome to my page ')
-);
-const listener = function (alert) {
-	addEventListener('click', listener);
-	BeforeUnloadEvent;
-
-	return listener('welcome to my page ');
-};
-
-listener();
